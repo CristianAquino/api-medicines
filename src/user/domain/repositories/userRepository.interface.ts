7f06e65f27431bb2dd5a9281940bdebd6c648af2
cristianaquino <ycristian10@gmail.com>
2024-08-08 16:59:39 -0500

    feat: user repository

---
+++ src/user/domain/repositories/userRepository.interface.ts
@@ -0,0 +1,20 @@
+import {
+  AddUserDTO,
+  FindAllUsersDTO,
+  UpdateByAdminDTO,
+  UpdateDataByUserDTO,
+} from '@user/infrastructure/controller/dto/user-in.dto';
+import {
+  ReturnAllUserData,
+  ReturnUserData,
+} from '@user/infrastructure/controller/dto/user-out.dto';
+
+export interface IUserRepository {
+  insert(user: AddUserDTO): Promise<void>;
+  findAll(findAllUserDTO: FindAllUsersDTO): Promise<ReturnAllUserData>;
+  findById(id: string): Promise<ReturnUserData>;
+  findOneByName(username: string): Promise<ReturnUserData>;
+  updateContentByUser(data: UpdateDataByUserDTO): Promise<number>;
+  updateContentByAdmin(id: string, content: UpdateByAdminDTO): Promise<void>;
+  deleteById(id: string): Promise<number>;
+}
---
+++ src/user/infrastructure/controller/enum/user.enum.ts
@@ -0,0 +1,6 @@
+export enum Role {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+export const RoleList = [Role.ADMIN, Role.USER];
