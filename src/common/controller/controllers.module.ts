fd08a8d6f4e5ab2ef854a3ea77a29768c16b5f96
cristianaquino <ycristian10@gmail.com>
2024-08-15 21:56:48 -0500

    feat: add product controller

--- src/common/controller/controllers.module.ts
+++ src/common/controller/controllers.module.ts
@@ -2,10 +2,16 @@ import { AuthController } from '@auth/infrastructure/controller/auth.controller'
 import { CategoryController } from '@category/infrastructure/controller/category.controller';
 import { UsecaseProxyModule } from '@common/usecases-proxy/usecases-proxy.module';
 import { Module } from '@nestjs/common';
+import { ProductController } from '@product/infrastructure/controller/product.controller';
 import { UserController } from '@user/infrastructure/controller/user.controller';
 
 @Module({
   imports: [UsecaseProxyModule.register()],
-  controllers: [AuthController, UserController, CategoryController],
+  controllers: [
+    AuthController,
+    UserController,
+    CategoryController,
+    ProductController,
+  ],
 })
 export class ControllersModule {}
