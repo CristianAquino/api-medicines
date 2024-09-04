930157e7ff361b48826f7f2cc5611e97eb02969a
cristianaquino <ycristian10@gmail.com>
2024-09-04 17:44:00 -0500

    test: user test

---
+++ test/user/putUpdateDataUser.spec.ts
@@ -0,0 +1,64 @@
+import { ILogger } from '../../src/common/logger/logger.interface';
+import { IUserRepository } from '../../src/user/domain/repositories';
+import { PutUpdateDataUserUseCase } from '../../src/user/usecases';
+
+describe('Test put update data user', () => {
+  let putUpdateDataUserUseCase: PutUpdateDataUserUseCase;
+  let logger: ILogger;
+  let userRepository: IUserRepository;
+
+  const updated = {
+    id: 1,
+    username: 'test',
+  };
+
+  beforeAll(() => {
+    logger = {} as ILogger;
+    logger.log = jest.fn();
+    logger.warn = jest.fn();
+
+    userRepository = {} as IUserRepository;
+    userRepository.updateContentToUser = jest.fn();
+
+    putUpdateDataUserUseCase = new PutUpdateDataUserUseCase(
+      logger,
+      userRepository,
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(putUpdateDataUserUseCase).toBeDefined();
+  });
+
+  it('should return a warning message if not user updated', async () => {
+    (userRepository.updateContentToUser as jest.Mock).mockResolvedValue(0);
+
+    await expect(putUpdateDataUserUseCase.execute(updated)).rejects.toThrow(
+      'User not found, please check the information',
+    );
+    expect(userRepository.updateContentToUser).toHaveBeenCalledWith(updated);
+    expect(logger.log).not.toHaveBeenCalled();
+    expect(logger.warn).toHaveBeenCalledWith(
+      'PutUpdateDataUserUseCase',
+      'User not found, please check the information',
+    );
+  });
+
+  it('should return a success message if user updated', async () => {
+    (userRepository.updateContentToUser as jest.Mock).mockResolvedValue(1);
+
+    await expect(putUpdateDataUserUseCase.execute(updated)).resolves.toBe(
+      'User updated successfully',
+    );
+    expect(userRepository.updateContentToUser).toHaveBeenCalledWith(updated);
+    expect(logger.warn).not.toHaveBeenCalled();
+    expect(logger.log).toHaveBeenCalledWith(
+      'PutUpdateDataUserUseCase',
+      'User updated successfully',
+    );
+  });
+});
---
+++ test/user/putUpdateDataUserByAdmin.spec.ts
@@ -0,0 +1,91 @@
+import { ILogger } from '../../src/common/logger/logger.interface';
+import { IUserRepository } from '../../src/user/domain/repositories';
+import { PutUpdateDataUserByAdminUseCase } from '../../src/user/usecases';
+
+describe('Test put update data user by admin', () => {
+  let putUpdateDataUserByAdminUseCase: PutUpdateDataUserByAdminUseCase;
+  let logger: ILogger;
+  let userRepository: IUserRepository;
+
+  const updated = {
+    id: 1,
+    username: 'test',
+  };
+
+  beforeAll(() => {
+    logger = {} as ILogger;
+    logger.log = jest.fn();
+    logger.warn = jest.fn();
+
+    userRepository = {} as IUserRepository;
+    userRepository.findById = jest.fn();
+    userRepository.updateContentToAdmin = jest.fn();
+
+    putUpdateDataUserByAdminUseCase = new PutUpdateDataUserByAdminUseCase(
+      logger,
+      userRepository,
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(putUpdateDataUserByAdminUseCase).toBeDefined();
+  });
+
+  it('should return a warning message if user not found', async () => {
+    (userRepository.findById as jest.Mock).mockResolvedValue(null);
+
+    await expect(
+      putUpdateDataUserByAdminUseCase.execute(updated),
+    ).rejects.toThrow('User not found, please check the information');
+
+    expect(userRepository.findById).toHaveBeenCalledWith(updated.id);
+    expect(userRepository.updateContentToAdmin).not.toHaveBeenCalled();
+    expect(logger.warn).toHaveBeenCalledWith(
+      'PutUpdateDataUserByAdminUseCase',
+      'User not found, please check the information',
+    );
+    expect(logger.log).not.toHaveBeenCalled();
+  });
+
+  it('should return a warning message if not user updated', async () => {
+    (userRepository.findById as jest.Mock).mockResolvedValue({
+      username: 'lorem',
+    });
+    (userRepository.updateContentToAdmin as jest.Mock).mockResolvedValue(0);
+
+    await expect(
+      putUpdateDataUserByAdminUseCase.execute(updated),
+    ).rejects.toThrow('User not found, please check the information');
+
+    expect(userRepository.findById).toHaveBeenCalledWith(updated.id);
+    expect(userRepository.updateContentToAdmin).toHaveBeenCalledWith(updated);
+    expect(logger.warn).toHaveBeenCalledWith(
+      'PutUpdateDataUserByAdminUseCase',
+      'User not found, please check the information',
+    );
+    expect(logger.log).not.toHaveBeenCalled();
+  });
+
+  it('should return a success message if user updated', async () => {
+    (userRepository.findById as jest.Mock).mockResolvedValue({
+      username: 'lorem',
+    });
+    (userRepository.updateContentToAdmin as jest.Mock).mockResolvedValue(1);
+
+    await expect(
+      putUpdateDataUserByAdminUseCase.execute(updated),
+    ).resolves.toBe('User updated successfully');
+
+    expect(userRepository.findById).toHaveBeenCalledWith(updated.id);
+    expect(userRepository.updateContentToAdmin).toHaveBeenCalledWith(updated);
+    expect(logger.warn).not.toHaveBeenCalled();
+    expect(logger.log).toHaveBeenCalledWith(
+      'PutUpdateDataUserByAdminUseCase',
+      'User updated successfully',
+    );
+  });
+});
