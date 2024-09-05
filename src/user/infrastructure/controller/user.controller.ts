e2cc861241469632f9e065fb8e8f8ee0b465cc53
cristianaquino <ycristian10@gmail.com>
2024-09-04 22:13:41 -0500

    feat: user controller

--- src/user/infrastructure/controller/user.controller.ts
+++ src/user/infrastructure/controller/user.controller.ts
@@ -1,5 +1,6 @@
-import { Roles } from '@common/decorators/roles.decorator';
+import { Roles } from '@common/decorators';
 import { ResponseErrorDTO } from '@common/dto';
+import { UserModel } from '@common/entities/models';
 import { JwtAuthGuard, RolesGuard } from '@common/guards';
 import { UseCaseProxy } from '@common/usecases-proxy/usecases-proxy';
 import { UsecaseProxyModule } from '@common/usecases-proxy/usecases-proxy.module';
@@ -16,6 +17,7 @@ import {
   Post,
   Put,
   Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -26,17 +28,21 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import {
-  AddUserUseCase,
+  CreateUserUseCase,
   DeleteUserUseCase,
   GetAllUsersUseCase,
+  PutUpdateDataUserByAdminUseCase,
   PutUpdateDataUserUseCase,
+  PutUpdatePasswordUserUseCase,
 } from '@user/usecases';
+import { Request } from 'express';
 import {
-  AddUserDTO,
+  CreateUserDTO,
   FindAllUsersDTO,
-  SWGReturnAllUserData,
-  SWGReturnUserData,
+  SWGAllUsersData,
+  SWGMessage,
   UpdateDataByUserDTO,
+  UpdateDataUserByAdminDTO,
 } from './dto';
 import { Role } from './enum/user.enum';
 
@@ -56,32 +62,36 @@ import { Role } from './enum/user.enum';
 })
 export class UserController {
   constructor(
-    @Inject(UsecaseProxyModule.ADD_USER_USECASE_PROXY)
-    private readonly addUserUsecaseProxy: UseCaseProxy<AddUserUseCase>,
+    @Inject(UsecaseProxyModule.CREATE_USER_USECASE_PROXY)
+    private readonly createUserUsecaseProxy: UseCaseProxy<CreateUserUseCase>,
     @Inject(UsecaseProxyModule.DELETE_USER_USECASE_PROXY)
     private readonly deleteUserUsecaseProxy: UseCaseProxy<DeleteUserUseCase>,
     @Inject(UsecaseProxyModule.GET_ALL_USERS_USECASE_PROXY)
     private readonly getAllUsersUsecaseProxy: UseCaseProxy<GetAllUsersUseCase>,
     @Inject(UsecaseProxyModule.PUT_UPDATE_DATA_USER_USECASE_PROXY)
     private readonly putUpdateDataUserUsecaseProxy: UseCaseProxy<PutUpdateDataUserUseCase>,
+    @Inject(UsecaseProxyModule.PUT_UPDATE_DATA_USER_BY_ADMIN_USECASE_PROXY)
+    private readonly putUpdateDataUserByAdminUsecaseProxy: UseCaseProxy<PutUpdateDataUserByAdminUseCase>,
+    @Inject(UsecaseProxyModule.PUT_UPDATE_PASSWORD_USER_USECASE_PROXY)
+    private readonly putUpdatePasswordUserUsecaseProxy: UseCaseProxy<PutUpdatePasswordUserUseCase>,
   ) {}
 
-  @Post('/add')
+  @Post('create')
   @Roles(Role.ADMIN)
-  @ApiBody({ type: AddUserDTO })
-  @ApiOperation({ summary: 'Add new user' })
+  @ApiBody({ type: CreateUserDTO })
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: HttpStatus.CREATED, type: SWGMessage })
   @HttpCode(HttpStatus.CREATED)
-  async addUser(@Body() addUser: AddUserDTO) {
-    const response = await this.addUserUsecaseProxy
+  async addUser(@Body() addUser: CreateUserDTO) {
+    const response = await this.createUserUsecaseProxy
       .getInstance()
       .execute(addUser);
     return response;
   }
-
-  @Get('/all')
+  @Get('all')
   @Roles(Role.ADMIN)
-  @ApiOperation({ summary: 'Get all users by user name' })
-  @ApiResponse({ status: HttpStatus.OK, type: SWGReturnAllUserData })
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: HttpStatus.OK, type: SWGAllUsersData })
   @HttpCode(HttpStatus.OK)
   async getAllUsers(@Query() findAllUsersByUsernameDTO: FindAllUsersDTO) {
     const response = await this.getAllUsersUsecaseProxy
@@ -89,21 +99,55 @@ export class UserController {
       .execute(findAllUsersByUsernameDTO);
     return response;
   }
-  @Put('/update')
+  @Put('update-user')
+  @Roles(Role.USER)
   @ApiOperation({ summary: 'Update user data' })
-  @ApiResponse({ status: HttpStatus.ACCEPTED, type: SWGReturnUserData })
+  @ApiResponse({ status: HttpStatus.ACCEPTED, type: SWGMessage })
   @ApiBody({ type: UpdateDataByUserDTO })
   @HttpCode(HttpStatus.ACCEPTED)
-  async putUpdateDataUser(@Body() updateDataByUserDTO: UpdateDataByUserDTO) {
+  async putUpdateDataUser(
+    @Req() request: Request,
+    @Body() updateDataByUserDTO: UpdateDataByUserDTO,
+  ) {
+    const user = request.user as UserModel;
     const response = await this.putUpdateDataUserUsecaseProxy
       .getInstance()
-      .execute(updateDataByUserDTO);
+      .execute({ ...updateDataByUserDTO, id: user.id });
     return response;
   }
-
-  @Delete('/delete/:id')
+  @Put('update-admin')
+  @Roles(Role.ADMIN)
+  @ApiBody({ type: UpdateDataUserByAdminDTO })
+  @ApiOperation({ summary: 'Update user data by admin' })
+  @ApiResponse({ status: HttpStatus.ACCEPTED, type: SWGMessage })
+  @HttpCode(HttpStatus.ACCEPTED)
+  async putUpdateDataUserByAdmin(
+    @Body() updateDataUserByAdminDTO: UpdateDataUserByAdminDTO,
+  ) {
+    const response = await this.putUpdateDataUserByAdminUsecaseProxy
+      .getInstance()
+      .execute(updateDataUserByAdminDTO);
+    return response;
+  }
+  @Post('generate-key')
+  @ApiOperation({ summary: 'generate key for update password user' })
+  @ApiResponse({ status: HttpStatus.CREATED, type: SWGMessage })
+  @HttpCode(HttpStatus.CREATED)
+  async generateKey(@Req() request: Request) {
+    const user = request.user as UserModel;
+    const response = await this.putUpdatePasswordUserUsecaseProxy
+      .getInstance()
+      .getKeyWithJwtToken({
+        id: user.id,
+        role: user.role,
+        available: user.available,
+      });
+    return response;
+  }
+  @Delete('delete/:id')
   @Roles(Role.ADMIN)
   @ApiOperation({ summary: 'Delete user' })
+  @ApiResponse({ status: HttpStatus.OK, type: SWGAllUsersData })
   @HttpCode(HttpStatus.OK)
   async deleteUser(@Param('id', ParseUUIDPipe) id: string) {
     const response = await this.deleteUserUsecaseProxy
