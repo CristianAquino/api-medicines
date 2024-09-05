d474c1b4ab7ba397b832ec7621efed638c31eff7
cristianaquino <ycristian10@gmail.com>
2024-09-04 22:14:16 -0500

    feat: auth controller

--- src/auth/infrastructure/controller/auth.controller.ts
+++ src/auth/infrastructure/controller/auth.controller.ts
@@ -5,11 +5,14 @@ import { JwtAuthGuard, LoginGuard } from '@common/guards';
 import { UseCaseProxy } from '@common/usecases-proxy/usecases-proxy';
 import { UsecaseProxyModule } from '@common/usecases-proxy/usecases-proxy.module';
 import {
+  Body,
   Controller,
   HttpCode,
   HttpStatus,
   Inject,
   Post,
+  Put,
+  Query,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -20,6 +23,11 @@ import {
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import {
+  SWGMessage,
+  UpdatePasswordDTO,
+} from '@user/infrastructure/controller/dto';
+import { PutUpdatePasswordUserUseCase } from '@user/usecases';
 import { Request } from 'express';
 import { LoginDTO } from './dto';
 
@@ -41,12 +49,15 @@ export class AuthController {
     private readonly loginUsecaseProxy: UseCaseProxy<LoginUseCase>,
     @Inject(UsecaseProxyModule.LOGOUT_USECASE_PROXY)
     private readonly logoutUsecaseProxy: UseCaseProxy<LogoutUseCase>,
+    @Inject(UsecaseProxyModule.PUT_UPDATE_PASSWORD_USER_USECASE_PROXY)
+    private readonly putUpdatePasswordUserUsecaseProxy: UseCaseProxy<PutUpdatePasswordUserUseCase>,
   ) {}
 
   @UseGuards(LoginGuard)
   @Post('login')
   @ApiBody({ type: LoginDTO })
   @ApiOperation({ description: 'Login' })
+  @ApiResponse({ status: HttpStatus.OK, type: SWGMessage })
   @HttpCode(HttpStatus.OK)
   async login(@Req() request: Request) {
     const user = request.user as UserModel;
@@ -61,10 +72,26 @@ export class AuthController {
   @Post('logout')
   @ApiCookieAuth()
   @ApiOperation({ description: 'Logout' })
+  @ApiResponse({ status: HttpStatus.OK, type: SWGMessage })
   @HttpCode(HttpStatus.OK)
   async logout(@Req() request: Request) {
     const cookie = await this.logoutUsecaseProxy.getInstance().execute();
     request.res.setHeader('Set-Cookie', cookie);
     return 'Logout successful';
+  }
+
+  @Put('update-password')
+  @ApiBody({ type: UpdatePasswordDTO })
+  @ApiOperation({ summary: 'Update user password' })
+  @ApiResponse({ status: HttpStatus.ACCEPTED, type: SWGMessage })
+  @HttpCode(HttpStatus.ACCEPTED)
+  async putUpdatePassword(
+    @Query('key') key: string,
+    @Body() updatePasswordDTO: UpdatePasswordDTO,
+  ) {
+    const response = await this.putUpdatePasswordUserUsecaseProxy
+      .getInstance()
+      .updatePassword({ key, password: updatePasswordDTO.newPassword });
+    return response;
   }
 }
