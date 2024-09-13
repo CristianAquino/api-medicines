f0238156ad9621a0808df5d01df7663a0b535d29
cristianaquino <ycristian10@gmail.com>
2024-09-12 21:58:18 -0500

    feat: order details controller

---
+++ src/order_details/infrastructure/controller/orderDetails.controller.ts
@@ -0,0 +1,49 @@
+import { ResponseErrorDTO } from '@common/dto';
+import { JwtAuthGuard, RolesGuard } from '@common/guards';
+import { UseCaseProxy } from '@common/usecases-proxy/usecases-proxy';
+import { UsecaseProxyModule } from '@common/usecases-proxy/usecases-proxy.module';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GetOrderDetailsByIdUseCase } from '@order_details/usecases';
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Controller('order details')
+@ApiTags('Order Details')
+@ApiCookieAuth()
+@ApiResponse({
+  status: HttpStatus.INTERNAL_SERVER_ERROR,
+  description: 'Internal error',
+  type: ResponseErrorDTO,
+})
+@ApiResponse({
+  status: HttpStatus.CONFLICT,
+  description: 'Conflict',
+  type: ResponseErrorDTO,
+})
+export class OrderDetailsController {
+  constructor(
+    @Inject(UsecaseProxyModule.GET_ORDER_DETAILS_BY_ID_USECASE_PROXY)
+    private readonly getOrderDetailsByIdUsecaseProxy: UseCaseProxy<GetOrderDetailsByIdUseCase>,
+  ) {}
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get all orders' })
+  @HttpCode(HttpStatus.OK)
+  async getOrderDetailsById(@Param('id', ParseIntPipe) id: number) {
+    return this.getOrderDetailsByIdUsecaseProxy.getInstance().execute(id);
+  }
+}
