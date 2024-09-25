c7baf9e3f95884b9d4177fd139677cdf630cf7d0
cristianaquino <ycristian10@gmail.com>
2024-09-24 22:19:52 -0500

    feat: order details repository

--- src/order_details/infrastructure/repositories/orderDetails.respository.ts
+++ src/order_details/infrastructure/repositories/orderDetails.respository.ts
@@ -1,5 +1,11 @@
 import { OrderDetail } from '@common/entities';
-import { OrderDetailsModel } from '@common/entities/models';
+import {
+  CustomerModel,
+  OrderDetailsModel,
+  OrderModel,
+  PaymentModel,
+  ProductModel,
+} from '@common/entities/models';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IOrderDetailsRepository } from '@order_details/domain/repositories/orderDetailsRepository.interface';
 import { Repository } from 'typeorm';
@@ -25,11 +31,59 @@ export class OrderDetailsRepository implements IOrderDetailsRepository {
     await this.orderDetailEntityRepository.save(od);
   }
   async getOrderDetailsById(id: number): Promise<any> {
+    // const details = await this.orderDetailEntityRepository
+    //   .createQueryBuilder('order_details')
+    //   .innerJoinAndSelect('order_details.orders', 'orders')
+    //   .innerJoinAndSelect('orders.product', 'product')
+    //   .innerJoinAndSelect('order_details.payment', 'payment')
+    //   .innerJoinAndSelect('order_details.customer', 'customer')
+    //   .where('order_details.id = :id', { id })
+    //   .getOne();
     const details = await this.orderDetailEntityRepository.findOne({
       where: { id: id },
-      relations: ['orders', 'payment', 'customer'],
+      relations: ['orders', 'orders.product', 'payment', 'customer'],
     });
     if (!details) return null;
-    return details;
+    return this.findOrderDetails(details);
+  }
+
+  private findOrderDetails(data: OrderDetailsModel) {
+    const od = new OrderDetailsModel();
+    od.id = data.id;
+    od.total_amount = data.total_amount;
+    od.sub_total = data.sub_total;
+    od.orders = data.orders.map((order) => this.findOrder(order));
+    od.customer = this.findCustomer(data.customer);
+    od.payment = this.findPayment(data.payment);
+    return od;
+  }
+  private findProduct(data: ProductModel): ProductModel {
+    const product = new ProductModel();
+    product.id = data.id;
+    product.name = data.name;
+    return product;
+  }
+  private findOrder(data: OrderModel): OrderModel {
+    const order = new OrderModel();
+    order.id = data.id;
+    order.quantity = data.quantity;
+    order.total = data.total;
+    order.unit_price = data.unit_price;
+    order.product = this.findProduct(data.product);
+    return order;
+  }
+  private findCustomer(data: CustomerModel): CustomerModel {
+    const customer = new CustomerModel();
+    customer.id = data.id;
+    customer.full_names = data.full_names ?? '';
+    customer.surnames = data.surnames ?? '';
+    customer.dni = data.dni;
+    return customer;
+  }
+  private findPayment(data: PaymentModel): PaymentModel {
+    const payment = new PaymentModel();
+    payment.id = data.id;
+    payment.type = data.type;
+    return payment;
   }
 }
